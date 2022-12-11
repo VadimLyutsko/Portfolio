b1d9a2574b1a4b9f5e33a4750604ec52830ee4bb
vadim <vadim_lyutsko@mail.ru>
2022-12-11 14:02:52 +0300

    First commit (add Header)

---
+++ src/main/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Main.module.css';
+
+export const Main = () => {
+    return (
+        <div className={style.main}>
+            <a href="">Главная</a>
+            <a href="">Что-то еще</a>
+            <a href="">Проекты</a>
+            <a href="">Что-то еще</a>
+            <a href="">На English</a>
+        </div>
+    );
+};
+
---
+++ src/nav/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Nav.module.css';
+
+export const Nav = () => {
+    return (
+        <div className={style.nav}>
+            <a href="">Главная</a>
+            <a href="">Что-то еще</a>
+            <a href="">Проекты</a>
+            <a href="">Что-то еще</a>
+            <a href="">На English</a>
+        </div>
+    );
+};
+

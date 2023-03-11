3fad0aefa369f12aaf9ea3821a2e90d6ea4f02a4
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-03-11 17:23:00 +0300

    Added text in the main and fixed header

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import style from './Main.module.css';
-import styleContainer from '../common/styles/Container.module.css';
 
 export const Main = () => {
     return (
         <div className={style.main}>
-            <div className={styleContainer.container}>
-                <div className={style.text}>
-                    <span>Some text in Main</span>
-                    <h1>Text H1</h1>
-                    <p>some link</p>
-                </div>
-                {/*<div className={style.photo}>*/}
-                {/*    <img src="https://i.pinimg.com/736x/a7/94/50/a79450796ef296bf93ef60971239a72f--beautiful-places.jpg" alt=""/>*/}
-                {/*</div>*/}
+            <div className={style.container}>
+                <h6>👋 Hello, My name is</h6>
+                <h1 className={style.myName}>Vadim Lyutsko</h1>
+                <p>-- The running line --</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda consectetur consequatur
+                </p>
+                <p>Download CV</p>
             </div>
+            <div className={style.bannerContainer}></div>
+            {/*<div className={style.photo}>*/}
+            {/*    <img src="https://i.pinimg.com/736x/a7/94/50/a79450796ef296bf93ef60971239a72f--beautiful-places.jpg" alt=""/>*/}
+            {/*</div>*/}
         </div>
     );
 };
--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -6,12 +6,12 @@ export const Nav = () => {
     return (
         <nav className={style.nav}>
             <ul>
-                <li className={style.navLi}><a href=""><img src={logo} alt=""/></a></li>
-                <li className={style.navLi}><a href="">Гл</a></li>
-                <li className={style.navLi}><a href="">Главная</a></li>
-                <li className={style.navLi}><a href="">Главная</a></li>
-                <li className={style.navLi}><a href="">Главная</a></li>
-                <li className={style.navLi}><a href="">Главная</a></li>
+                <li className={style.navLiLogo}><a href=""><img src={logo} alt=""/></a></li>
+                <li className={style.navLi}><a href="">Image 1</a></li>
+                <li className={style.navLi}><a href="">Image 2</a></li>
+                <li className={style.navLi}><a href="">Image 3</a></li>
+                <li className={style.navLi}><a href="">Image 4</a></li>
+                <li className={style.navLi}><a href="">Image 5</a></li>
             </ul>
         </nav>
 

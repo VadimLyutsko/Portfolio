95291350393aa50a57c1cef4c62958362f52f0c6
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-07-15 17:57:03 +0300

    Added information about my projects and fixed some unimportant bug.

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import style from './Projects.module.css';
 import {Project} from './project/Project';
 import {FormattedMessage} from 'react-intl';
 
 export type ProjectPropsType = {
     linkToTheProject: string
-    description: string
+    description: ReactElement | string
     title: string
     icon: string
     id: number
@@ -17,33 +17,34 @@ export const Projects = () => {
             id: 1,
             title: 'Social network',
             icon: 'https://i.imgur.com/8QeXll7.jpeg',
-            description: 'My little social network',
+            description: <p><FormattedMessage id="social_network"/></p>,
             linkToTheProject: 'https://vadimlyutsko.github.io/The-Way-of-the-Samurai/'
         },
         {
+
             id: 2,
             title: 'Todolist',
             icon: 'https://camo.githubusercontent.com/16be8365c473ac26fefb5281597db1e66fce08a43c43e5bd4b67b115b44f82d4/68747470733a2f2f7777772e6c65616465727461736b2e72752f696d616765732f61396438623534383565343962626164373463303236666434656531353063302e6a7067',
-            description: 'My big todolist ',
+            description: <p><FormattedMessage id="big_todolists_project"/></p>,
             linkToTheProject: 'https://vadimlyutsko.github.io/Great-Todolist/'
         },
         {
             id: 3,
             title: 'My homeworks',
             icon: 'https://oneclassblog.com/wp-content/uploads/2017/11/board-928381_1280.jpg',
-            description: 'Some text about my homeworks ',
+             description: <p><FormattedMessage id="my_homeworks"/></p>,
             linkToTheProject: 'https://vadimlyutsko.github.io/My-Homeworks/'
         },
         {
             id: 4, title: 'BloggerPlatform',
             icon: 'https://i.pinimg.com/originals/f0/2b/98/f02b984d8be0c58277b05a3287d48998.png',
-            description: ' One of the training projects (in progress) ',
+            description: <p><FormattedMessage id="bloggers_platform"/></p>,
             linkToTheProject: 'https://vadimlyutsko.github.io/BloggerPlatform/'
         },
         {
             id: 5, title: 'MiniTodo',
             icon: 'https://pbs.twimg.com/media/EGCVYsYWoAA-jMC?format=jpg&name=4096x4096',
-            description: ' Test assignment for hiring ',
+            description: <p><FormattedMessage id="test_task"/></p>,
             linkToTheProject: 'https://vadimlyutsko.github.io/ToDo_For_Mindbox/'
         },
 
@@ -52,20 +53,20 @@ export const Projects = () => {
     return (
         <div id={'projects'} className={style.projectsBlock}>
 
-                <h2 className={style.title}><FormattedMessage id="my_projects"/></h2>
+            <h2 className={style.title}><FormattedMessage id="my_projects"/></h2>
 
-                <div className={style.projects}>
-                    {
-                        projectState.map(skill =>
-                            <div key={skill.id}>
-                                <Project linkToTheProject={skill.linkToTheProject}
-                                         description={skill.description}
-                                         title={skill.title}
-                                         icon={skill.icon}
-                                         id={skill.id}/>
-                            </div>)
-                    }
-                </div>
+            <div className={style.projects}>
+                {
+                    projectState.map(skill =>
+                        <div key={skill.id}>
+                            <Project linkToTheProject={skill.linkToTheProject}
+                                     description={skill.description}
+                                     title={skill.title}
+                                     icon={skill.icon}
+                                     id={skill.id}/>
+                        </div>)
+                }
+            </div>
         </div>
     );
 };
--- src/i18n/messages.js
+++ src/i18n/messages.js
@@ -1,4 +1,4 @@
-import { LOCALES } from './locales'
+import {LOCALES} from './locales'
 
 export const messages = {
     [LOCALES.ENGLISH]: {
@@ -11,24 +11,29 @@ export const messages = {
         about_me: 'Great!!! You have visited my presentation mini-site, where you can get acquainted with the technologies that I own and with the projects that I have worked on. I will be glad to work with you (for you). 👩🏻‍💻 👩🏻‍💻 👩🏻‍💻 :) ',
         download_cv: 'Download CV',
         // меню
-        skills:'Skills',
-        projects:'Projects',
-        my_projects:'My projects',
-        contacts:'Contacts',
-        footer:'I\'m Online',
+        skills: 'Skills',
+        projects: 'Projects',
+        my_projects: 'My projects',
+        contacts: 'Contacts',
+        footer: 'I\'m Online',
         // навыки
-        my_skills:'My skills',
-        html_skill :' I learned HTML on my own for about 1 month here ',
-        css_skill :'I learned CSS on my own  here ',
-        sass_skill :'and Sass here',
-        js_skill :'Here I studied JavaScript on my own for a year',
-        js_skill_course :'and graduated from this course',
-        react_skill :'React is the library which I studied ( a course of 12 months) here ',
-        redux_skill :'Redux is a library which I studied with Redux in the It-incubator',
-        typescript_skill :'Typescript is a language that I studied with React in an It-incubator',
-        storybook_skill :'Storybook is a tool that I learned together with React in the It Incubator',
-        react_query_skill:'React jquery is a library that I am studying now',
-
+        my_skills: 'My skills',
+        html_skill: ' I learned HTML on my own for about 1 month here ',
+        css_skill: 'I learned CSS on my own  here ',
+        sass_skill: 'and Sass here',
+        js_skill: 'Here I studied JavaScript on my own for a year',
+        js_skill_course: 'and graduated from this course',
+        react_skill: 'React is the library which I studied ( a course of 12 months) here ',
+        redux_skill: 'Redux is a library which I studied with Redux in the It-incubator',
+        typescript_skill: 'Typescript is a language that I studied with React in an It-incubator',
+        storybook_skill: 'Storybook is a tool that I learned together with React in the It Incubator',
+        react_query_skill: 'React jquery is a library that I am studying now',
+        //проекты
+        social_network: 'A small social network developed on class components and transformed into functional components. I used Redux, React-router-dom v 6, Axioms, Formic, etc.',
+        big_todolists_project:'My big project, which dragged on throughout the course and in which we dug deep into relaxation, touching on non-obvious points, including productivity. I used Redux, Typescript, React-router-dom v 6, Axioms, Formic, Material UI, Storybook, etc.',
+        my_homeworks:'A project consisting of three levels of complexity, on which I worked out filtering, sorting, server requests and response processing, error handling and other small chips.',
+        bloggers_platform:'A training site for the exchange of educational information with the possibility of editing and full moderation, I\'m working on CSS on it, is currently under development.',
+        test_task:'A test task for a device to some company, a small to-do list, made for a quick hand. But it\'s done :)'
 
     },
 
@@ -41,26 +46,30 @@ export const messages = {
             'Я РАЗРАБОТЧИК ИНТЕРФЕЙСА',
         about_me: 'ОТЛИЧНО!!! Вы посетили мой презентационный мини-сайт, на котором сможете ознакомиться с технологиями, которыми я владею, и с проектами, над которыми я работал. Буду рад порабать с Вами (на Вас) 👩🏻‍💻 👩🏻‍💻 👩🏻‍💻 :) ',
 
-                download_cv: 'Скачать CV',
+        download_cv: 'Скачать CV',
         // меню
-        skills:'Навыки',
-        my_skills:'Мои навыки',
-        projects:'Проекты',
-        my_projects:'Мои проекты',
-        contacts:'Контакты',
-        footer:'Я в сети',
+        skills: 'Навыки',
+        my_skills: 'Мои навыки',
+        projects: 'Проекты',
+        my_projects: 'Мои проекты',
+        contacts: 'Контакты',
+        footer: 'Я в сети',
         // навыки
-        html_skill :'Я самостоятельно изучал HTML здесь около 1 месяц здесь',
-        css_skill :'Я самостоятельно изучал CSS  здесь ',
-        sass_skill :'и Sass здесь ',
-        js_skill :'Я самостоятельно изучал JavaScript около года здесь',
-        js_skill_course :'и окончил этот курс',
-        react_skill :'React -это та библиотека, которую я изучал (12 месяцев) здесь ',
-        redux_skill :'Redux - это библиотека, которую я изучал вместе с Redux в It-инкубаторе',
-        typescript_skill :'Typescript - это язык, который я изучал вместе с React в It-инкубаторе',
-        storybook_skill :'Storybook- это инструмент , который я изучал вместе с React в It-инкубаторе',
-        react_query_skill:'React query - это библиотека, которую я сейчас изучаю 📑📑  ',
-
-
+        html_skill: 'Я самостоятельно изучал HTML здесь около 1 месяц здесь',
+        css_skill: 'Я самостоятельно изучал CSS  здесь ',
+        sass_skill: 'и Sass здесь ',
+        js_skill: 'Я самостоятельно изучал JavaScript около года здесь',
+        js_skill_course: 'и окончил этот курс',
+        react_skill: 'React -это та библиотека, которую я изучал (12 месяцев) здесь ',
+        redux_skill: 'Redux - это библиотека, которую я изучал вместе с Redux в It-инкубаторе',
+        typescript_skill: 'Typescript - это язык, который я изучал вместе с React в It-инкубаторе',
+        storybook_skill: 'Storybook- это инструмент , который я изучал вместе с React в It-инкубаторе',
+        react_query_skill: 'React query - это библиотека, которую я сейчас изучаю 📑📑  ',
+        //проекты
+        social_network: 'Небольшая социальная сеть, разработанная на классовых компонентах и переписанная позже на функциональные компоненты. Использовал Redux, Typescript, React-router-dom v 6, Axioms, Formic и др.',
+        big_todolists_project:'Мой большой проект, который тянулся на протяжении всего курса и на котором мы глубоко копались в релаксе, затрагивая неочевидные моменты, в том числе производительность. Использовал Redux, Typescript, React-router-dom v 6, Axioms, Formic, Material UI, Storybook и др.',
+        my_homeworks:'Проект, состоящий из трех уровней сложности, на которых я отрабатывал фильтрации, сортировки, запросы на сервер и обработку ответов, работу с ошибками и другие мелкие фишки.',
+        bloggers_platform:'Учебный сайт для обмена учебной информацией с возможностью редактирования и полноценной модерацией, отрабатываю на нем CSS, сейчас находится в процессе разработки.',
+        test_task:'Тестовое задание для устройства в какую-то компанию, небольшой список дел, сделанный на быструю руку. Но ведь сделанный :)'
     }
 }
